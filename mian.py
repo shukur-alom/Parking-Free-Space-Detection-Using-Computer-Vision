626a3f3182f9010ad49835bf9f08810554f1c839
shukur-alom <shukuralom1234@gmail.com>
2024-03-30 12:03:54 +0600

    draw polygon  and show it

--- mian.py
+++ mian.py
@@ -1,28 +1,52 @@
 import cv2
+import numpy as np
 
+# List to store points
+points = []
+poligon = []
+
+
+# Function to draw polygon
+def draw_polygon(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        points.append((x, y))
+
+
+# Create a black image, a window and bind the function to window
 cap = cv2.VideoCapture("Media/3214448-hd_1920_1080_25fps.mp4")
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", draw_polygon)
 
-if cap.isOpened() == False:
-    print("Error opening video file")
-
-
-while cap.isOpened():
-
-
+while 1:
     ret, frame = cap.read()
-    if ret == True:
+    if not ret:
+        break
+    mask = np.zeros_like(frame)
+    for i in poligon:
+        # Assuming 'frame' is your original image and 'i' is your polygon points
+        cv2.fillPoly(mask, [np.array(i)], (124, 255, 112))
 
-        cv2.imshow("Frame", frame)
+    frame = cv2.addWeighted(mask, 0.2, frame, 1, 0)
 
+    for x, y in points:
+        cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
 
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            break
+    cv2.imshow("image", frame)
 
-    else:
+    wail_key = cv2.waitKey(1)
+
+    if wail_key == ord("s"):
+        if len(points) > 0:
+            poligon.append(points)
+            points = []
+
+    elif wail_key == ord("r"):
+        try:
+            poligon.pop()
+        except:
+            pass
+    elif wail_key & 0xFF == ord("q"):
         break
 
-
 cap.release()
-
-# Closes all the frames
 cv2.destroyAllWindows()

d24f896eed6aaa39d871268ea3a18c05f99e3265
shukur-alom <shukuralom1234@gmail.com>
2024-03-30 12:20:15 +0600

    find_polygon_center

---
+++ functions.py
@@ -0,0 +1,7 @@
+def find_polygon_center(points):
+    '''Find the center of a polygon given its points.'''
+    x_coords = [p[0] for p in points]
+    y_coords = [p[1] for p in points]
+    center_x = int(sum(x_coords) / len(points))
+    center_y = int(sum(y_coords) / len(points))
+    return (center_x, center_y)
